3dd81289dbce8983edb870c1146ef306230476a1
Kirill Kryuchkov <kirill.kruchkov@gmail.com>
2017-09-20 09:02:29 +0100

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, View } from 'react-native'
+import App from './src/App'
+
+export default class Enzyme2Json3RNTest extends Component {
+  render() {
+    return (
+      <View>
+        <App />
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('Enzyme2Json3RNTest', () => Enzyme2Json3RNTest)
---
+++ jest-setup.js
@@ -0,0 +1,6 @@
+require.requireActual('react-native/jest/setup')
+
+const Adapter = require('enzyme-adapter-react-16')
+const Enzyme = require('enzyme')
+
+Enzyme.configure({ adapter: new Adapter() })
